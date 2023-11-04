1854f2807e051fce249ded2cf06ab0f78476ae02
Madelyn Arsenault <madelyn.arsenault@gmail.com>
2023-11-04 01:19:55 -0500

    more navbar stuff implementing hamburger button

--- src/app/components/navigation/navbar/Navbar.tsx
+++ src/app/components/navigation/navbar/Navbar.tsx
@@ -1,14 +1,24 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
 import Madsokami from '@/app/logos/Madsokami.png'
-
-
+import styles from '@/app/styles/navbar.module.scss'
+import Link from 'next/link'
+import {AiOutlineMenu} from 'react-icons/ai'
+import { useState } from 'react'
 
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleNav = () => {
+    setMenuOpen(!menuOpen)
+  }
+  
   return (
-    <nav className='fixed w-full h-24 shadow-xl bg-transparent'>
-      <div className='flex justify-between items-center h-full w-full px-4 2xl:px12'>
+    <nav className={styles.navbar}>
+      <div className={styles.navlogo}>
+        <Link href='/'>
         <Image 
         src={Madsokami}
         width={205}
@@ -16,8 +26,29 @@ const Navbar = () => {
         className='cursor-pointer'
         alt="mads navbar logo cute"
         />
-        <div>right side</div>
+        </Link>
+        <div className={styles.navMainDiv}>
+          <ul className={styles.navUnordered}>
+            <Link href='/about' className={styles.navLists}>
+            <li>About Me</li>
+            </Link>
+            <Link href='/projects' className={styles.navLists}>
+            <li>Projects</li>
+            </Link>
+            <Link href='/contact' className={styles.navLists}>
+            <li>Contact</li>
+            </Link>
+          </ul>
+        </div>
+        <div onClick={handleNav} className={styles.navHamburgerButton}>
+          <AiOutlineMenu size={37} />
+        </div>
       </div>
+      {menuOpen ?
+      <div className={styles.navHamburgerOpen}></div>
+       :
+      <div className={styles.navHamburgerClosed}></div>
+      }
     </nav>
   )
 }
